ae1ac94c2b18c36e846727bcff3a1fd78afdd129
nicohman <nicohman@demenses.net>
2018-09-01 16:38:34 -0700

    Move enums into seperate mod

---
+++ src/bin/args/mod.rs
@@ -0,0 +1,73 @@
+pub mod rargs {
+    use structopt::StructOpt;
+    #[derive(StructOpt, Debug)]
+    #[structopt(name = "raven")]
+    pub enum Raven {
+        #[structopt(name = "load", about = "Load a complete theme")]
+        Load { theme: String },
+        #[structopt(name = "new", about = "Create a new theme")]
+        New { name: String },
+        #[structopt(name = "modify",
+                    about = "Open the currently edited themes's option in $EDITOR")]
+        Modify { name: String },
+        #[structopt(name = "delete", about = "Delete a theme")]
+        Delete { name: String },
+        #[structopt(name = "info", about = "Print info about the theme being currently edited")]
+        Info {},
+        #[structopt(name = "refresh", about = "Load last loaded theme")]
+        Refresh {},
+        #[structopt(name = "install", about = "Install a theme from ThemeHub repo")]
+        Install { name: String },
+        #[structopt(name = "add", about = "Add option to current theme")]
+        Add { option: String, name: String },
+        #[structopt(name = "rm", about = "Remove an option from edited theme")]
+        Rm { name: String },
+        #[structopt(name = "edit", about = "Edit theme")]
+        Edit { name: String },
+        #[structopt(name = "menu", about = "Show theme menu")]
+        Menu {},
+        #[structopt(name = "manage", about = "Manage online themes")]
+        ManageO(Manage),
+        #[structopt(name = "cycle", about = "Control cycle daemon")]
+        CycleD(Cycle),
+    }
+    #[derive(StructOpt, Debug)]
+    pub enum Manage {
+        #[structopt(name = "export", about = "Export a theme to a tarball")]
+        Export { name: String },
+        #[structopt(name = "import", about = "Import a theme from a tarball")]
+        Import { name: String },
+        #[structopt(name = "publish", about = "Publish an account online")]
+        Publish { name: String },
+        #[structopt(name = "create", about = "Create an account")]
+        Create {
+            name: String,
+            pass1: String,
+            pass2: String,
+        },
+        #[structopt(name = "meta", about = "Edit an online theme's metadata")]
+        Meta {
+            name: String,
+            mtype: String,
+            value: String,
+        },
+        #[structopt(name = "delete_user",
+                    about = "Delete an online user's profile and owned themes")]
+        DUser { pass: String },
+        #[structopt(name = "logout", about = "Log out of your user profile")]
+        Logout {},
+        #[structopt(name = "unpublish", about = "Delete an online theme")]
+        Unpublish { name: String },
+        #[structopt(name = "login", about = "Log in to an user's account")]
+        Login { name: String, pass: String },
+    }
+    #[derive(StructOpt, Debug)]
+    pub enum Cycle {
+        #[structopt(name = "start", about = "Start the daemon")]
+        Start {},
+        #[structopt(name = "check", about = "Check if daemon is running")]
+        Check {},
+        #[structopt(name = "stop", about = "Stop the daemon")]
+        Stop {},
+    }
+}
--- src/bin/raven.rs
+++ src/bin/raven.rs
@@ -11,6 +11,8 @@ extern crate tar;
 extern crate multipart;
 mod ravenserver;
 mod ravenlib;
+mod args;
+use args::rargs::*;
 use ravenlib::rlib::*;
 use ravenserver::ravens::*;
 extern crate hyper;
@@ -18,74 +20,6 @@ extern crate reqwest;
 #[macro_use]
 extern crate structopt;
 use structopt::StructOpt;
-#[derive(StructOpt, Debug)]
-#[structopt(name = "raven")]
-enum Raven {
-    #[structopt(name = "load", about = "Load a complete theme")]
-    Load { theme: String },
-    #[structopt(name = "new", about = "Create a new theme")]
-    New { name: String },
-    #[structopt(name = "modify", about = "Open the currently edited themes's option in $EDITOR")]
-    Modify { name: String },
-    #[structopt(name = "delete", about = "Delete a theme")]
-    Delete { name: String },
-    #[structopt(name = "info", about = "Print info about the theme being currently edited")]
-    Info {},
-    #[structopt(name = "refresh", about = "Load last loaded theme")]
-    Refresh {},
-    #[structopt(name = "install", about = "Install a theme from ThemeHub repo")]
-    Install { name: String },
-    #[structopt(name = "add", about = "Add option to current theme")]
-    Add { option: String, name: String },
-    #[structopt(name = "rm", about = "Remove an option from edited theme")]
-    Rm { name: String },
-    #[structopt(name = "edit", about = "Edit theme")]
-    Edit { name: String },
-    #[structopt(name = "menu", about = "Show theme menu")]
-    Menu {},
-    #[structopt(name = "manage", about = "Manage online themes")]
-    ManageO(Manage),
-    #[structopt(name = "cycle", about = "Control cycle daemon")]
-    CycleD(Cycle),
-}
-#[derive(StructOpt, Debug)]
-enum Manage {
-    #[structopt(name = "export", about = "Export a theme to a tarball")]
-    Export { name: String },
-    #[structopt(name = "import", about = "Import a theme from a tarball")]
-    Import { name: String },
-    #[structopt(name = "publish", about = "Publish an account online")]
-    Publish { name: String },
-    #[structopt(name = "create", about = "Create an account")]
-    Create {
-        name: String,
-        pass1: String,
-        pass2: String,
-    },
-    #[structopt(name = "meta", about = "Edit an online theme's metadata")]
-    Meta {
-        name: String,
-        mtype: String,
-        value: String,
-    },
-    #[structopt(name = "delete_user", about = "Delete an online user's profile and owned themes")]
-    DUser { pass: String },
-    #[structopt(name = "logout", about = "Log out of your user profile")]
-    Logout {},
-    #[structopt(name = "unpublish", about = "Delete an online theme")]
-    Unpublish { name: String },
-    #[structopt(name = "login", about = "Log in to an user's account")]
-    Login { name: String, pass: String },
-}
-#[derive(StructOpt, Debug)]
-enum Cycle {
-    #[structopt(name = "start", about = "Start the daemon")]
-    Start {},
-    #[structopt(name = "check", about = "Check if daemon is running")]
-    Check {},
-    #[structopt(name = "stop", about = "Stop the daemon")]
-    Stop {},
-}
 //Structure that holds theme data, to be stored in a theme folder.
 fn main() {
     if check_init() {
@@ -171,33 +105,7 @@ fn interpet_args() {
     }
 
 }
-/*
-fn process_manage_args(args: Vec<String>) {
-    let cmd2 = (&args[2]).as_ref();
-    if !check_args_cmd(args.len() - 3, cmd2) {
-        println!("Not enough arguments for {}", &cmd2);
-        ::std::process::exit(64);
-    }
-    match cmd2 {
-        "export" => ravens::export(&args[3]),
-        "import" => ravens::import(&args[3]),
-        "publish" => ravens::upload_theme((&args[3]).to_string()),
-        "create" => {
-            ravens::create_user(
-                (&args[3]).to_string(),
-                (&args[4]).to_string(),
-                (&args[5]).to_string(),
-            )
-        },
-        "meta" => ravens::pub_metadata((&args[3]).to_string(),(&args[4]).to_string(),(&args[5]).to_string()),
-        "unpublish" => ravens::unpublish_theme((&args[3]).to_string()),
-        "login" => ravens::login_user((&args[3]).to_string(), (&args[4]).to_string()),
-        "logout" => ravens::logout(),
-        "delete_user" => ravens::delete_user((&args[3]).to_string()),
-        _ => println!("Manage requires a subcommand. Run raven help for more info."),
-    }
-}
-*/
+
 fn print_info(editing: String) {
     let options = fs::read_dir(get_home() + "/.config/raven/themes/" + &editing)
         .expect("Couldn't read themes")
@@ -219,7 +127,6 @@ fn modify_file(editing: String, file: &str) {
         "Couldn't run $EDITOR",
     );
 }
-fn manage_daemon(command: &str) {}
 fn show_menu(menu_command: String) {
     let mut theme_list = String::new();
     let mut entries = fs::read_dir(get_home() + "/.config/raven/themes")
@@ -261,34 +168,6 @@ fn show_menu(menu_command: String) {
 
 }
 
-fn print_help() {
-    println!("Commands:");
-    println!("help : show this screen");
-    println!("load [theme] : load a complete theme");
-    println!("new [theme] : create a new theme");
-    println!("delete [theme] : delete a theme");
-    println!("refresh : load last loaded theme");
-    println!("edit [theme] : initialize editing [theme]");
-    println!("modify [option] : open the currently edited themes's [option] in $EDITOR");
-    println!("add [option] [file] : add option to current theme");
-    println!("rm [option] : remove option from current theme");
-    println!("cycle {{check|start|stop}} : manage theme cycling daemon");
-    println!("info : print info about the theme being currently edited");
-    println!("menu : show theme menu");
-    println!("install [name] : try to install a theme from the online repo");
-    println!("manage [subcommand] : manage online theme publishing with subcommands");
-    println!("      - import [archive] : import an exported theme");
-    println!("      - export [theme] : export target theme to a tarball");
-    println!("      - create [username] [password] [repeat password] : create a new user");
-    println!("      - unpublish [name] : delete a published theme from repo");
-    println!("      - login [username] [password] : login to a user profile");
-    println!("      - publish [theme] : when logged in, publish a theme online");
-    println!("      - logout : logout of a user profile");
-    println!(
-        "      - meta [theme] [type] [value] : update the metadata of a published theme, either `screen`(a url to a screenshot) or `description`"
-    );
-    println!("      - delete_user [password] : delete your user profile and any owned themes.");
-}
 fn get_home() -> String {
     return String::from(env::home_dir().unwrap().to_str().unwrap());
 }
